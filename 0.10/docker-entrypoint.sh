8ffb4a625c09a28452336d1f86b29a536f23f65e
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-02-21 20:54:44 +1100

    Initial commit: Dockerfile with confs and entrypoint

---
+++ 0.10/docker-entrypoint.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Optional ENV variables:
+# * ZK_MASTER: the zookeeper cluster master
+# * KAFKA_HOST: the external ip for the container, e.g. `docker-machine ip \`docker-machine active\``
+# * KAFKA_PORT: the external port for Kafka, e.g. 9092
+# * ZK_CHROOT: the zookeeper chroot that's used by Kafka (without / prefix), e.g. "kafka"
+# * LOG_RETENTION_HOURS: the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week)
+# * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
+# * NUM_PARTITIONS: configure the default number of log partitions per topic
+
+# Get local internal docker ip
+MY_IP=`awk 'NR==1 {print $1}' /etc/hosts` 
+echo "### Starting .... Docker IP is $MY_IP"
+
+# Set the kafka external host and port
+if [ ! -z "$KAFKA_HOST" ]; then
+    echo "### Kafka advertised host: $KAFKA_HOST"
+    if grep -q "^advertised.host.name" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/#(advertised.host.name)=(.*)/\1=$KAFKA_HOST/g" $KAFKA_HOME/config/server.properties
+    else
+        echo "advertised.host.name=$KAFKA_HOST" >> $KAFKA_HOME/config/server.properties
+    fi
+fi
+if [ ! -z "$KAFKA_PORT" ]; then
+    echo "### Kafka advertised port: $KAFKA_PORT"
+    if grep -q "^advertised.port" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/#(advertised.port)=(.*)/\1=$KAFKA_PORT/g" $KAFKA_HOME/config/server.properties
+    else
+        echo "advertised.port=$KAFKA_PORT" >> $KAFKA_HOME/config/server.properties
+    fi
+fi
+
+# Set the zookeeper master
+if [ ! -z "$ZK_MASTER" ]; then
+    echo "### Existing master zookeeper: $ZK_MASTER"
+    sed -r -i "s~(zookeeper.connect)=(.*)~\1=${ZK_MASTER}~g" $KAFKA_HOME/config/server.properties
+else
+    # Set the zookeeper chroot
+    if [ ! -z "$ZK_CHROOT" ]; then
+        # Wait for zookeeper to start up
+        until /usr/share/zookeeper/bin/zkServer.sh status; do
+            sleep 0.1
+        done
+
+        # Create the chroot node
+        echo "### Creating Zookeeper CHROOT $ZK_CHROOT"
+        echo "create /$ZK_CHROOT \"\"" | /usr/share/zookeeper/bin/zkCli.sh || {
+            echo "can't create chroot in zookeeper, exit"
+            exit 1
+        }
+
+        echo "### New master zookeeper: localhost:2181/$ZK_CHROOT"
+        sed -r -i "s/(zookeeper.connect)=(.*)/\1=localhost:2181\/$ZK_CHROOT/g" $KAFKA_HOME/config/server.properties
+    fi
+fi
+
+# Allow specification of log retention policies
+if [ ! -z "$LOG_RETENTION_HOURS" ]; then
+    echo "log retention hours: $LOG_RETENTION_HOURS"
+    sed -r -i "s/(log.retention.hours)=(.*)/\1=$LOG_RETENTION_HOURS/g" $KAFKA_HOME/config/server.properties
+fi
+if [ ! -z "$LOG_RETENTION_BYTES" ]; then
+    echo "log retention bytes: $LOG_RETENTION_BYTES"
+    sed -r -i "s/#(log.retention.bytes)=(.*)/\1=$LOG_RETENTION_BYTES/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Configure the default number of log partitions per topic
+if [ ! -z "$NUM_PARTITIONS" ]; then
+    echo "default number of partition: $NUM_PARTITIONS"
+    sed -r -i "s/(num.partitions)=(.*)/\1=$NUM_PARTITIONS/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Enable/disable auto creation of topics
+if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
+    echo "auto.create.topics.enable: $AUTO_CREATE_TOPICS"
+    echo "auto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
+fi
+
+# Run Kafka
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
---
+++ README.md
@@ -0,0 +1,18 @@
+# Maestrano Kafka
+Docker image for Apache Kafka
+
+## Build this docker image
+
+* Build the image
+`docker build -t docker-kafka ./0.10`
+
+* Check that the image built
+`docker images`
+
+## Run a cluster
+
+* Run the master image
+`docker run -p 2181:2181 -p 9092:9092 --env ZK_CHROOT=mno --env KAFKA_HOST=localhost --env KAFKA_PORT=9092 docker-kafka`
+
+* Run a slave image
+`docker run -p 9093:9092 --env ZK_MASTER=localhost:2181/mno --env ZK_CHROOT=mno --env KAFKA_HOST=localhost --env KAFKA_PORT=9092 docker-kafka`
